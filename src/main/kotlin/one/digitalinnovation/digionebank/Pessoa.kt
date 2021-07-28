443b1e8121d05f235d9bf66c71e84f535cba2536
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-28 14:45:16 -0300

    updated to apply the private modifier to set function at cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,22 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var name: String = "Rafael"
-    var cpf: String = "300.713.318-16"
+     var cpf: String = "300.713.318-16"
+    private set
+
+
+//    inner class Endereco {
+//        var rua: String = "Rua Teste"
+//    }
+
 }
 
 fun main() {
     val rafael = Pessoa()
+//    rafael.cpf = "4567"
 
     println(rafael.name)
     println(rafael.cpf)
+
+//    println(rafael.Endereco().rua)
 }
