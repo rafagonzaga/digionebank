7885849144f0e9ebe017d7546ee5aa5ed6e1826a
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-28 18:39:19 -0300

    updated to make person and employee abstract class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
+    val salario: BigDecimal
+) : Pessoa(nome, cpf) {
+    abstract fun calculoAuxilio();
+
+}
--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var name: String = "Rafael"
-     var cpf: String = "300.713.318-16"
-    private set
-
-//    constructor()
-
-    fun pessoaInfo() = "$name e $cpf"
-}
-
-fun main() {
-    val rafael = Pessoa()
-
-    println(rafael.pessoaInfo())
-}
-
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
