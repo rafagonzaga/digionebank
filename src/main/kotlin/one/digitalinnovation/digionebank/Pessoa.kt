f3f1e6caecf32786504f275c43580e73260416c8
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-28 15:10:59 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,19 +5,13 @@ class Pessoa {
      var cpf: String = "300.713.318-16"
     private set
 
+//    constructor()
 
-//    inner class Endereco {
-//        var rua: String = "Rua Teste"
-//    }
-
+    fun pessoaInfo() = "$name e $cpf"
 }
 
 fun main() {
     val rafael = Pessoa()
-//    rafael.cpf = "4567"
 
-    println(rafael.name)
-    println(rafael.cpf)
-
-//    println(rafael.Endereco().rua)
+    println(rafael.pessoaInfo())
 }
