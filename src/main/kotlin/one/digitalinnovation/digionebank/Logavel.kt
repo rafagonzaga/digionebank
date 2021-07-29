520e12a23d9db304e5efba44ada9e9bea4f10fbe
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-29 17:32:47 -0300

    Finalização do curso de Fundamentos de Orientação a Objetos com Kotlin. Bootcamp Santander | Dio

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Logavel.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovation.digionebank
+
+// Toda interface é abstrata
+interface Logavel {
+    fun login(): Boolean
+}
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,8 +3,10 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria do Carmo", "30377155888", 5000.0)
+    val maria = Gerente("Maria do Carmo", "30377155888", 5000.0, "senha123")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
 
